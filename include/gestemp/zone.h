0d60b0c3dac868ef863573ab2c711ae01d9b88e8
scr-17 <santiago.castror@uanl.edu.mx>
2025-10-26 18:11:22 -0600

    added zone.h, header for the 'zone' module

---
+++ include/gestemp/zone.h
@@ -0,0 +1,16 @@
+#ifndef GESTEMP_ZONE_H
+#define GESTEMP_ZONE_H
+#include <gestemp/fan.h>
+
+typedef struct {
+    int zoneId;
+    char zoneName[50];
+    float temperatureThreshold;
+    FanStatus fanStatus;
+    FanType fanType;
+    int fanNum;
+} Zone;
+
+//void zoneRegistration();
+
+#endif //GESTEMP_ZONE_H
