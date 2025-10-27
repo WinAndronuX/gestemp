9cf62da13ed8332a4d683034c00e22725e1eb8f8
WinAndronuX <salazarsanchezluisdaniel@hotmail.com>
2025-10-26 22:15:07 -0600

    Agregar: thread para cada segundo Agregar: Llamar a la funcion tempsensorInit para inicializar el sensor de temperatura

--- include/gestemp/utils.h
+++ include/gestemp/utils.h
@@ -1,6 +1,17 @@
 #ifndef GESTEMP_UTILS_H
 #define GESTEMP_UTILS_H
 #include <stdio.h>
+#include <stdlib.h>
+
+#ifdef _WIN32
+#include <Windows.h>
+#define sleepSec(s) Sleep(s * 1000)
+#define clearConsole() system("cls")
+#else
+#include <unistd.h>
+#define sleepSec(s) sleep(s)
+#define clearConsole() system("clear")
+#endif
 
 void fileValidation (FILE **pf);
 
--- src/main.c
+++ src/main.c
@@ -1,10 +1,32 @@
 #include <stdio.h>
+#include <pthread.h>
+#include <gestemp/utils.h>
+#include <gestemp/tempsensor.h>
 
 
+void *executeEverySecond() {
+
+    while (1) {
+
+        printf("Ejecutando..\n");
+
+        sleepSec(1);
+    }
+}
 
 int main() {
 
+    tempsensorInit();
 
+    clearConsole();
+    pthread_t thread;
+    const int error = pthread_create(&thread, NULL, executeEverySecond, NULL);
+    if (error) {
+        printf("Error al crear el hilo de ejecucion.\nSaliendo...\n");
+        return 1;
+    }
+
+    pthread_join(thread, NULL);
 
     return 0;
 }
